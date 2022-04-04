e32b541956a7ffb683e63c207acc49dc12ebc7ab
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-04 15:00:15 +0100

    Implements Services and controllers

--- src/main/java/tn/esb/lmad/library_api/Controllers/BookController.java
+++ src/main/java/tn/esb/lmad/library_api/Controllers/BookController.java
@@ -1,11 +1,22 @@
 package tn.esb.lmad.library_api.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tn.esb.lmad.library_api.Domains.Book;
 import tn.esb.lmad.library_api.Services.BookService;
+
+import java.util.List;
 
 @RestController
 public class BookController {
     @Autowired
     private BookService bookServ;
+
+    @GetMapping("")
+    public ResponseEntity<List<Book>> allBooks()
+    {
+        bookServ.getAllBooks();
+    }
 }
--- src/main/java/tn/esb/lmad/library_api/Services/BookService.java
+++ src/main/java/tn/esb/lmad/library_api/Services/BookService.java
@@ -1,11 +1,24 @@
 package tn.esb.lmad.library_api.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import tn.esb.lmad.library_api.Domains.Book;
 import tn.esb.lmad.library_api.Repositories.BookRepository;
+
+import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
     private BookRepository bookRepos;
+     //implementer une méthode qui retourne tous les books
+    public ResponseEntity<List<Book>> getAllBooks()
+    {
+        List<Book> result=bookRepos.findAll();
+        if(result.isEmpty())
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
