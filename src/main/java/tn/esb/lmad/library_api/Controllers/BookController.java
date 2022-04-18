af98d35499f32a0cee4780ba06c56f34a6ab4f8e
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-18 14:13:18 +0100

    Implements Services and controllers

--- src/main/java/tn/esb/lmad/library_api/Controllers/BookController.java
+++ src/main/java/tn/esb/lmad/library_api/Controllers/BookController.java
@@ -2,26 +2,34 @@ package tn.esb.lmad.library_api.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tn.esb.lmad.library_api.Domains.Book;
 import tn.esb.lmad.library_api.Services.BookService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
+//The url of this controller is
+//http://localhost:9592/books
 public class BookController {
     @Autowired
     private BookService bookServ;
 
     @GetMapping("/all")
+    //or
+   // @RequestMapping(value="/all",method=RequestMethod.GET)
+    //the is
+    //http://localhost:9592/books/all
     public ResponseEntity<List<Book>> allBooks()
     {
        return bookServ.getAllBooks();
     }
-    @PostMapping("/")
-    public ResponseEntity<Book> newBook(Book b)
+    @PostMapping("/add")
+    //via the RequestBody annotation we tell the server to look for
+    // the book value on the body of the request
+    public ResponseEntity<Book> newBook(@Valid @RequestBody Book b)
     {
         return bookServ.addBook(b);
     }
--- src/main/java/tn/esb/lmad/library_api/Controllers/MemberController.java
+++ src/main/java/tn/esb/lmad/library_api/Controllers/MemberController.java
@@ -1,10 +1,12 @@
 package tn.esb.lmad.library_api.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tn.esb.lmad.library_api.Services.MemberService;
 
 @RestController
+@RequestMapping("/members")
 public class MemberController {
     @Autowired
     private MemberService memberServ;
