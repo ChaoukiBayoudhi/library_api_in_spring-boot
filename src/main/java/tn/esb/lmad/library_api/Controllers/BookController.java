21d3244399abb058755fa5bf7d22a3aa23f96ac5
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-18 13:00:51 +0100

    Implements Services and controllers

--- src/main/java/tn/esb/lmad/library_api/Controllers/BookController.java
+++ src/main/java/tn/esb/lmad/library_api/Controllers/BookController.java
@@ -3,6 +3,7 @@ package tn.esb.lmad.library_api.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tn.esb.lmad.library_api.Domains.Book;
 import tn.esb.lmad.library_api.Services.BookService;
@@ -18,5 +19,10 @@ public class BookController {
     public ResponseEntity<List<Book>> allBooks()
     {
        return bookServ.getAllBooks();
+    }
+    @PostMapping("/")
+    public ResponseEntity<Book> newBook(Book b)
+    {
+        return bookServ.addBook(b);
     }
 }
